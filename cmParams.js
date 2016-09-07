cd8bd2d1011bf8805db3c08d1e838b3317f3fcc7
Hgjj <hgjjmail@gmail.com>
2016-09-07 15:58:05 +0200

    massiv refactor because scalajs compiler ultimatum

--- cmParams.js
+++ cmParams.js
@@ -5,7 +5,7 @@ var fileCMParams = {"bodyParts" : [
 		 "partTransform" : [ 1, 1, 0, 0, 0 ],
 		 "components" : [
 			{"imageRef" : "Backgrounds/Blue/big.png",
-				"transform" : [ 1, 1, 0, 0, 0 ],
+				"transform" : [ 1, 1, 0, -15, -40 ],
 				"colorVariable" : "None",
 				"z_layer" : 0,
 				"condition" : [ "always" ],
@@ -57,18 +57,75 @@ var fileCMParams = {"bodyParts" : [
 		 "partTransform" : [ 1, 1, 0, 0, 0 ],
 		 "components" : [
 			{"imageRef" : "Misc/colors/colors.png",
-				"transform" : [ 1, 1, 0, 0, 0 ],
+				"transform" : [ 1, 1, 0, -13, -38 ],
 				"colorVariable" : "None",
 				"z_layer" : 0,
 				"condition" : [ "always" ],
 				"name" : "Misc/colors/colors.png"
 			}
 		] 
+	},
+
+	{ "partName" : "ShapePart",
+		 "category" : "ShapeTest",
+		 "partZ" : 0,
+		 "partTransform" : [ 1, 1, 0, 0, 0 ],
+		 "components" : [
+			{"imageRef" : "None",
+				"transform" : [ 1, 1, 0, 0, 0 ],
+				"colorVariable" : "None",
+				"z_layer" : 0,
+				"condition" : [ "always" ],
+				"name" : "Nothing"
+			},
+			{"points" : [ [ { "x" : -108.43632156016537, "y" : -34.330222348542975 } ], [ { "x" : -89.29319316633675, "y" : 22.709593879223632 }, { "x" : 2.5274039328867843, "y" : 27.903623946721098 }, { "x" : 2.5274039328867843, "y" : 27.903623946721098 } ] ],
+				"transform" : [ 1, 1, 50, 463, 135 ],
+				"colorVariables" : [ 			{"boundColor" : "A color",
+				"constantColor" : "#ff430a",
+				"alpha" : 0.9995999932289124
+			}, 			{"boundColor" : "A color",
+				"constantColor" : "#080d0a",
+				"alpha" : 0.9995999932289124
+			} ],
+				"z_layer" : 2,
+				"condition" : [ "always" ],
+				"lineWidth" : 5,
+				"showSurface" : true,
+				"lineJoin" : "miter",
+				"closed" : true,
+				"deltaLink" : 			{"key" : 1,
+				"slider" : "None",
+				"position" : 0
+			},
+				"name" : "Test"
+			},
+			{"points" : [ [ { "x" : -108.43632156016537, "y" : -34.330222348542975 } ], [ { "x" : -89.29319316633675, "y" : 22.709593879223632 }, { "x" : 2.5274039328867843, "y" : 27.903623946721098 }, { "x" : 2.5274039328867843, "y" : 27.903623946721098 } ] ],
+				"transform" : [ 1, 1, 50, -120, 165 ],
+				"colorVariables" : [ 			{"boundColor" : "None",
+				"constantColor" : "#ffffff",
+				"alpha" : 1
+			}, 			{"boundColor" : "None",
+				"constantColor" : "#ffffff",
+				"alpha" : 1
+			} ],
+				"z_layer" : 2,
+				"condition" : [ "always" ],
+				"lineWidth" : 5,
+				"showSurface" : true,
+				"lineJoin" : "miter",
+				"closed" : true,
+				"deltaLink" : 			{"key" : 1,
+				"slider" : "Tr",
+				"position" : 10
+			},
+				"name" : "TestTr"
+			}
+		] 
 	}
 ],
-"colors" : [  ],
-"choices" : [ 0, 1 ],
-"selected" : [ 1, -1, -1, 1 ],
-"sliders" : [  ],
-"date" : 1473232912309
+"colors" : [ "#98FF30" ],
+"choices" : [ 0, 1, 0 ],
+"selected" : [ 2, 0, 0, 1 ],
+"sliders" : [ 0 ],
+"date" : 1473256585058
 };
